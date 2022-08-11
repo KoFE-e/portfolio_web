b650d58a2674a44d19fa32710e0cbbb1a74126a6
KoFE <petrovykh03@gmail.com>
2022-08-11 19:27:22 +0500

    form validation

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ gulp.task('styles', function() {
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
@@ -41,6 +42,12 @@ gulp.task('html', function () {
     return gulp.src("src/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('php', function() {
+    return gulp.src("src/php/**/*")
+        .pipe(gulp.dest("dist/php"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function () {
@@ -68,4 +75,4 @@ gulp.task('images', function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images', 'php'));
--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,66 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+var inputs = document.querySelectorAll('input, textarea');
+var countinputs = 0;
+var validation = true;
+
+$(document).ready(function(){
+    $('#contacts-form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            text: {
+                required: true,
+                minlength: 10
+            },
+            check: "required"
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите не менее {0}-ух символов")
+            },
+            email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+            },
+            text: {
+                required: "Пожалуйста, введите сообщение",
+                minlength: jQuery.validator.format("Введите не менее {0}-и символов")
+            },
+            check: "Пожалуйста, поставьте галочку, если Вы согласны с политикой конфиденциальности"
+        }
+    });
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        inputs = document.querySelectorAll('input, textarea');
+        inputs.forEach(item => {
+            if(item.classList.contains('valid')) {
+                countinputs++;
+            };
+        });
+        validation = countinputs == 4;
+        if (validation) {
+            $.ajax({
+                type: "POST",
+                url: "php/mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function() {
+                $(this).find("input").val("");
+                $('form').trigger('reset');
+                validation = true;
+            });
+        };
+        countinputs = 0;
+        return false;
+    });
+});
